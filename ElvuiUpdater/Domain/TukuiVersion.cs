4f48e1e930559d713da199fd553532850e2c09b7
Kortunov Vladimir <sobad.raizer@gmail.com>
2023-06-01 11:12:31 +0300

    implement tukiu web site api to check and download updates

---
+++ ElvuiUpdater/Domain/TukuiVersion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElvuiUpdater.Domain
+{
+    public class TukuiVersion
+    {
+        public int id { get; set; }
+        public string slug { get; set; }
+        public string author { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+        public string version { get; set; }
+        public string changelog_url { get; set; }
+        public string ticket_url { get; set; }
+        public string git_url { get; set; }
+        public List<string> patch { get; set; }
+        public string last_update { get; set; }
+        public string web_url { get; set; }
+        public string donate_url { get; set; }
+        public string small_desc { get; set; }
+        public string screenshot_url { get; set; }
+        public List<string> directories { get; set; }
+    }
+
+}
